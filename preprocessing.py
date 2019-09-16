4a9108061f3384d14d1ec6b780162a0261b2f873
kreeteeekah <35298230+kreeteeekah@users.noreply.github.com>
2019-09-16 10:26:57 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,26 +1,26 @@
 import cv2
-import numpy as np
-from matplotlib import pyplot as plt
 
 def main():
     
-    for i in range(1,5):
-        
-        #for reading the images in the dataset
-        imgpath = "C:\\Users\\MUJ\\Desktop\\Minor Project\\test1\\"+"palm"+str(i)+".png"
+    for i in range(1,3):
+        #for reading the images from dataset
+        imgpath = "F:\\leap dataset\\leapGestRecog\\binarized dataset\\single prediction\\"+"c_or_palm"+str(i)+".png"
     
-        outpath = "C:\\Users\\MUJ\\Desktop\\Minor Project\\test2\\"+"palm"+str(i)+".png"
+        outpath = "F:\\leap dataset\\leapGestRecog\\binarized dataset\\binarized_prediction\\"+"c_or_palm"+str(i)+".png"
         
         #While reading the image it is read as gray scaled
         #Change below line to do any operation on the image
         img = cv2.imread(imgpath, 0)
+        
         median = cv2.medianBlur(img,5)
         threshold = cv2.adaptiveThreshold(median,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
             cv2.THRESH_BINARY,11,2)
-        
+        #The parameter 'op' is the name of the window in which the image is opened
         #cv2.imshow('op',img)
-        plt.show(threshold)
-        cv2.imwrite(outpath, threshold)
+        #to resize the image to 28x28 pixel
+        write_img = cv2.resize(threshold, (128,128))
+
+        cv2.imwrite(outpath, write_img)
     
     cv2.waitKey(0)
     #cv2.destroyAllWindows()
